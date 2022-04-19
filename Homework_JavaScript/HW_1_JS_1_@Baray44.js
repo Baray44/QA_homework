836066e4ac11a9b2e1e5007b33ad4e5d7b4c6f88
Baray44 <79219538+Baray44@users.noreply.github.com>
2022-04-19 18:32:46 +0300

    Update HW_1_JS_1_@Baray44.js

--- Homework_JavaScript/HW_1_JS_1_@Baray44.js
+++ Homework_JavaScript/HW_1_JS_1_@Baray44.js
@@ -7,7 +7,7 @@ if (age < age_2) {
     console.log("You don't have access cause your age is " + age + " It's less then ")
 } else if (age >= age_2 && age < age_3) {
     console.log("Welcome !")
-} else if (age > age_3) {
+} else if (age >= age_3) {
     console.log("Keep calm and look Culture channel")
 } else {
     console.log("Technical work")
